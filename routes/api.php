9583627c46172ae4bcb6a6f0c83055e1af7ef81e
Dev 01 <dev01@cecremge.org.br>
2020-12-01 09:45:19 -0300

    começando a estrutura definida pelo gabriela2003_00

--- routes/api.php
+++ routes/api.php
@@ -6,28 +6,3 @@ Route::group(['prefix' => 'sessions'], function () {
     Route::get('/', 'SessionController@index')->middleware('jwt.auth');
     Route::post('/', 'SessionController@store');
 });
-
-Route::group(['prefix' => 'pessoasFisicas'], function () {
-    Route::get('/', 'PessoaFisicaController@index');
-    Route::get('/{cpf}', 'PessoaFisicaController@buscarPorCpf');
-});
-
-Route::group(['prefix' => 'cooperativas'], function () {
-    Route::get('/', 'CooperativaController@index');
-    Route::get('/{codigoCooperativa}/funcionarios', 'CooperativaController@funcionarios');
-    Route::get('/{codigoCooperativa}/funcionariosPorTipo', 'CooperativaController@funcionariosPorTipo');
-});
-
-Route::group(['prefix' => 'sistemasCecremge'], function () {
-    Route::get('/', 'SistemaCecremgeController@index');
-    Route::get('/{codigoSistema}/{contaDominio}', 'SistemaCecremgeController@permissaoContaDominio');
-});
-
-Route::group(['prefix' => 'pessoasJuridicas'], function () {
-    Route::get('/', 'PessoaJuridicaController@index');
-});
-
-Route::group(['prefix' => 'tiposPessoasFisicas'], function () {
-    Route::get('/', 'TipoPessoaFisicaController@index');
-    Route::get('/{codigoTipo}', 'TipoPessoaFisicaController@pessoasFisicasPorTipo');
-});
