36ea9d0dddc0897f84403cc555865687b30724b4
Dev 01 <dev01@cecremge.org.br>
2020-12-02 14:55:03 -0300

    adicionando o código da cooperativa na trilha de auditoria

--- database/migrations/2020_11_12_124620_create_trilha_de_auditoria_table.php
+++ database/migrations/2020_11_12_124620_create_trilha_de_auditoria_table.php
@@ -11,6 +11,7 @@ class CreateTrilhaDeAuditoriaTable extends Migration
         Schema::create('TrilhaDeAuditoria_API', function (Blueprint $table) {
             $table->id();
             $table->string('contaDominio');
+            $table->integer('codigoCooperativa');
             $table->datetime('tokenGeradoEm');
             $table->datetime('tokenExpiradoEm');
         });
